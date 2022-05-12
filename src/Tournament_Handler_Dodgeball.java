25d4b35eab312c405ad1bf0150726579cdf1b18c
zoozol123 <filiplazewski02@gmail.com>
2022-05-13 00:01:03 +0200

    zrobilem ta tabelke dwuwymairowa

--- src/Tournament_Handler_Dodgeball.java
+++ src/Tournament_Handler_Dodgeball.java
@@ -5,7 +5,20 @@ public class Tournament_Handler_Dodgeball {
 	Team_Manager team_manager = new Team_Manager();
 	int number_of_matches;
 	int number_of_teams;
-	int i = 0;
+	//int i = 0, j, k = 1;
+	int[][] tab = new int[team_manager.teamSize()][team_manager.teamSize()];//tablica zeby kazdy
+																		  //zagral z kazdym
+	for(int i = 0; i < team_manager.teamSize(); i++) 						
+		for(int j = 0; j < team_manager.teamSize(); j++) //najpierw cala zerami
+			tab[i][j] = 0;
+							
+	for(i = 0, k = 1; i < team_manager.teamSize(); i++, k++) 
+		for(j = k; j < team_manager.teamSize(); j++)  //pozniej jedynkami w opowiednich miejscach
+			tab[i][j] = 1;
+		
+		
+		
+	
 	
 	Tournament_Handler_Dodgeball()
 	{
